124449aecc4e40130fd31a8e258c6acaf9d2b46b
alannarisse <angrypirate@gmail.com>
2020-12-04 11:36:19 -0800

    loayout mixes

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -6,7 +6,7 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3500,
+    autoplaySpeed: 500,
     infinite: true,
     dots: false,
     draggable: false
